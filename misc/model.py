edd047ad14bbca9d71f8f165b8d23624153f8f01
svs <sailesh101195@gmail.com>
2017-11-06 21:24:47 -0600

    Initial setup for running experiments

--- misc/model.py
+++ misc/model.py
@@ -1,8 +1,27 @@
 from torch.autograd import Variable
-import torchvison.models as models
+import torchvision.models as models
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
+
+class Net(nn.Module):
+    def __init__(self, opts):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 13 * 13, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, opts.num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 13 * 13)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 def ixvr(input_layer, bias_val=0):
     nn.init.xavier_normal(input_layer.weight);
